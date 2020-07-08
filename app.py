26a1957d8eae900c015be11a9cd41441d05cf2ce
hemantsirsat <hemantsirsat40@gmail.com>
2020-07-08 23:01:30 +0530

    API works. (Done in Console)

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST','GET'])
+def index():
+    return render_template('home.html')
+
+if __name__ =="__main__":
+    app.run(port="7001",debug=True)
