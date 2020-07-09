7214e6346e0ed5d22675c22ce235df262d397f36
hemantsirsat <hemantsirsat40@gmail.com>
2020-07-09 23:44:25 +0530

    Background change acc. to temp

--- templates/app.js
+++ templates/app.js
@@ -1,20 +1,31 @@
 $("#check").click(function(){
-    console.log('Hello')
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=7de7313c646f15c056b5daaeda647efa",function(data){
+    var city = document.getElementById("city").value;
+    var n = city;
+    city = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7de7313c646f15c056b5daaeda647efa"
+    $.getJSON(city,function(data){
         var place = data.name;
-        place = "City: " + place;
         $('.name').append(place);
-
+        
         console.log(data);
 
         var temp = data.main.temp;
-        temp = (temp-32)*(5/9);
-        temp = "Temperature: " + temp;
-        var feels_like = data.main.feels_like;
-        feels_like = (feels_like-32)*(5/9);
-        feels_like = "Feels Like: " + feels_like;
+        temp = Number(temp) - 273;
+        temp.toFixed(2);
+        var temp1 = temp;
+        temp = "Temperature: " + String(temp);
+        var desc = data.weather[0].description;
 
         $('.Current-Temperature').append(temp);
-        $('.Feels-like').append(feels_like);
+        $('.description').append(desc);
+
+        if(Number(temp1) < 20){
+            document.body.style.backgroundImage = "url('images/cold.jpeg')"; 
+        }
+        else if(Number(temp1) < 30){
+            document.body.style.backgroundImage = "url('images/mild.jpeg')";                    
+        }
+        else{
+            document.body.style.backgroundImage = "url('images/hot.jpeg')"; 
+        }
     });
 });
