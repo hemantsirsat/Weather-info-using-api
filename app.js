83b4dd9eba3591f8aa414c3f887410be049abe88
hemantsirsat <hemantsirsat40@gmail.com>
2020-07-10 15:24:12 +0530

    Glitches Fixed for repeat

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ $("#check").click(function(){
     city = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7de7313c646f15c056b5daaeda647efa"
     $.getJSON(city,function(data){
         var place = data.name;
-        $('.name').append(place);
-        
-        console.log(data);
-
         var temp = data.main.temp;
+        var desc = data.weather[0].description;
+        
         temp = Number(temp) - 273;
-        temp.toFixed(2);
+        temp = temp.toFixed(2);
         var temp1 = temp;
         temp = "Temperature: " + String(temp);
-        var desc = data.weather[0].description;
 
+        $("p").empty();
+
+        $('.name').append(place);
         $('.Current-Temperature').append(temp);
         $('.description').append(desc);
 
